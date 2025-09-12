624aeb2185c27afc3e2549a9f1f04764dd1644eb
Antisgoat <78632148+Antisgoat@users.noreply.github.com>
2025-09-11 23:03:23 -0400

    refactor: stabilize build and lint setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css";
-import "./styles/mbs.theme.css";
-import GlobalErrorBoundary from "./components/GlobalErrorBoundary";
-import EnvBanner from "./components/EnvBanner";
-import { runPreviewChecks } from "./components/PreviewHealthCheck";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import './styles/mbs.theme.css';
+import GlobalErrorBoundary from './components/GlobalErrorBoundary';
+import EnvBanner from './components/EnvBanner';
+import { runPreviewChecks } from './components/PreviewHealthCheck';
+import Skeleton from './components/Skeleton';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <GlobalErrorBoundary>
         <EnvBanner />
-        <App />
+        <React.Suspense fallback={<Skeleton />}>
+          <App />
+        </React.Suspense>
         {import.meta.env.DEV && runPreviewChecks()}
       </GlobalErrorBoundary>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
