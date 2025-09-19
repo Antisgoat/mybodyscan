79f877c3921b23b016640aab9671a433802ffcd4
Antisgoat <78632148+Antisgoat@users.noreply.github.com>
2025-09-19 13:00:27 -0400

    chore: pin simple-swizzle and update engines

---
+++ functions/src/firebase.ts
@@ -0,0 +1,10 @@
+import { getApps, initializeApp } from "firebase-admin/app";
+
+if (!getApps().length) {
+  initializeApp();
+}
+
+export { getAuth } from "firebase-admin/auth";
+export { getFirestore, Timestamp, FieldValue } from "firebase-admin/firestore";
+export { getAppCheck } from "firebase-admin/app-check";
+export { getStorage } from "firebase-admin/storage";
